9f2645525e6ed74006ddbc025a1dfc3e4a4ff26b
Rushiraj Wani <rajwani2003@gmail.com>
2024-12-03 11:26:45 +0530

    Created Basic questions folder, added .gitignore file and create a sum of 2 no's file.

---
+++ Basic-questions/01_sumOfTwoNumbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "Enter value of a: ";
+    cin >> a;
+    cout << "Enter value of b: ";
+    cin >> b;
+
+    int sum = a + b;
+    cout << "Sum of a & b is: " << sum << "\n";
+}
