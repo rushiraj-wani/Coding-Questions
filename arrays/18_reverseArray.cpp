2991bd83c9f3f00b74f9bb4420a0bb48eb6efada
Rushiraj Wani <rajwani2003@gmail.com>
2025-03-06 22:28:24 +0530

    Add function to reverse an array and print its contents

---
+++ arrays/18_reverseArray.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void reversedArray(int arr[], int n){
+    int start = 0;
+    int end = n-1;
+
+    while(start < end){
+        swap(arr[start], arr[end]); 
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original Array: ";
+    printArray(arr, n);
+
+    reversedArray(arr, n);
+
+    cout << "Reversed Array: ";
+    printArray(arr, n);
+
+
+    return 0;
+}
